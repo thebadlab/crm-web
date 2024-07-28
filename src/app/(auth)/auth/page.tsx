ef08e6d2b8c4f452f5c95c89664afd742b0ecbac
Nick Risinger <boozy044@gmail.com>
2024-07-28 16:27:26 +0300

    add main pages

---
+++ src/app/(auth)/auth/page.tsx
@@ -0,0 +1,3 @@
+export default function AuthPage() {
+  return <main>Auth Page</main>;
+}
---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Auth',
+};
+
+export default function AuthLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return <div>{children}</div>;
+}
